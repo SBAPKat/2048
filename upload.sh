63099df2f87ed5eb1f75cb947ef4ea0da85fdbd7
SBAPKat <sbapkat@gmail.com>
2019-05-01 15:55:39 +0000

    new upload.sh

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-read -p "Enter name of file to upload, if you want to do a global upload, type *" name
+read -p "Enter name of file to upload (with extention), if you want to do a global upload, type * :" name
 if [ "$name" = "*" ]
 then
     echo "uploading everything"
