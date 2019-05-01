1a95ccadde487dd9414226afa997314be83e06fd
SBAPKat <sbapkat@gmail.com>
2019-05-01 14:27:39 +0000

    changed docstrings and score system

--- 2048.py
+++ 2048.py
@@ -20,53 +20,51 @@ def exceptonormal():
 
 
 def move(deway):
-    """fonction qui effectue le déplacement et la combinaison des cases dans le sens demandé
-    on utilise deux boucles for avec d'autres boucles imbriquées,on vérifie la variable deway
-    afin de choisir les bonne variable et la bonne boucle, elle renvoie la variable mouvement
-    qui indique si au moins une case à été déplacée"""
+    """fonction qui gere le deplacement, deway est la varibel contenant le sens voulu, les variable i et k sont respectivement les lignes
+    et les colones du tableau, afin de verifier si l'on a perdu, on renvoie la varible mouvement qui indique si il a au moins une case a bougée"""
     mouvement = 0 # on initialise la variable qui informe si une case a bougée
     if deway == "down" or deway =="right":
-        a1 = 3 # valeur utilisée dans les boucles for qui suivent
-        a2 = -1 # elle varie en fonction de la manière dont on veut parcourir
+        borneinf = 3 # valeur utilisée dans les boucles for qui suivent
+        bornesup = -1 # elle varie en fonction de la manière dont on veut parcourir
         # le tableau
-        a3 = -1
-        b = 1
+        pas = -1
+        sens = 1 #Varie en fonction du sens de lecture : tab[i][k] est la case déplacée
+        #tab[i+sens][k] ou  tab[i][k+sens] est la case sur laquelle tab[i][k] va se déplacer.
     else:
-        a1 = 1
-        a2 = 5
-        a3 = 1
-        b = -1
+        borneinf = 1
+        bornesup = 5
+        pas = 1
+        sens = -1
     if deway == "down" or deway == "up":
         for passage in range(4): #tableau de 5*5 donc 5 passage
-            for i in range(a1,a2,a3): # minimum, maximum, pas
+            for i in range(borneinf,bornesup,pas): # minimum, maximum, pas
                 for k in range(5):
-                    if tab[i + b][k] == tab[i][k] and tab[i][k] not in exceptions and tab[i][k] != 0:
-                        tab[i + b][k] = 2 * tab[i][k] + 1
+                    if tab[i + sens][k] == tab[i][k] and tab[i][k] not in exceptions and tab[i][k] != 0:
+                        tab[i + sens][k] = 2 * tab[i][k] + 1
                         # on fait fois deux pour combiner les cases, puis on rajoute 1 afin de ne pas
                         # fusionne deux fois une case lors d'un passage
                         tab[i][k] = 0
                         mouvement = 1 #un mouvement a été effectué, mouvement est donc vrai
-                    if tab[i + b][k] == 0 and tab[i][k] !=0:
-                        tab[i + b][k] = tab[i][k]
+                    if tab[i + sens][k] == 0 and tab[i][k] !=0:
+                        tab[i + sens][k] = tab[i][k]
                         tab[i][k] = 0
                         mouvement = 1
         exceptonormal()  #on retire les 1 ajoutés précedemment
         return mouvement
     else:
         for passage in range(4):
-            for k in range(a1,a2,a3):
+            for k in range(borneinf,bornesup,pas):
                 for i in range(5):
-                    if tab[i][k + b] == tab[i][k] and tab[i][k] not in exceptions and tab[i][k] != 0:
-                        tab[i][k + b] = 2 * tab[i][k] + 1  # on fait fois deux pour combiner les cases
+                    if tab[i][k + sens] == tab[i][k] and tab[i][k] not in exceptions and tab[i][k] != 0:
+                        tab[i][k + sens] = 2 * tab[i][k] + 1  # on fait fois deux pour combiner les cases
                         tab[i][k] = 0
                         mouvement = 1
-                    if tab[i][k + b] == 0 and tab[i][k] !=0:
-                        tab[i][k + b] = tab[i][k]
+                    if tab[i][k + sens] == 0 and tab[i][k] !=0:
+                        tab[i][k + sens] = tab[i][k]
                         tab[i][k] = 0
                         mouvement =1
         exceptonormal()
         return mouvement
-
 
 def images_load():
     """Charge les images et les stocke dans un dictionnaire qu'elle renvoie"""
@@ -88,43 +86,40 @@ def vic_def(mouvement):
         for y in range(5):
             if tab[x][y] != 0:
                 number_cases = number_cases + 1
-<<<<<<< HEAD
         if tab[x][y] == 2048: #si un 2048 est présent victoire
             return 1 #on retourne donc 1
     if number_cases == 25 and mouvement == 0: #si aucun mouvement n'a été effectué et que le tableau est plein
-	    return 2 #on renvoie 2
-=======
+            return 2 #on renvoie 2
             if tab[x][y] == 2048: #si un 2048 est présent victoire
                 return 1 #on retourne donc 1
     if number_cases == 25 and mouvement == 0: #si aucun mouvement n'a été effectué et que le tableau est plein
         return 2 #on renvoie 2
->>>>>>> 95030d4985f9bf24b7ca30121a236a1e20f3cc20
     return 0 #sinon 0
 
-def random_case():
+def random_case(score):
     """Place une case (2 ou 4 au hasard avec 2 ayant une probabilité de 0.75 et 4 de 0.25) dans TAB à une position aléatoire"""
     number = randr(0, 4)  # Choisit un nombre au hasard afin de déterminer si un 2 ou un 4 apparait
     number_x, number_y = randr(0, 5), randr(0, 5)  # Choisit les coordonnées dans le tableau au hasard
     if tab[number_x][number_y] == 0:  # Vérifie si la case est vide
         if number == 0:  # if et else servent ici à déterminer 2 ou 4 à partir de la valeur aléatoire
             number = 4
+            score = score + 4
         else:
             number = 2
+            score = score + 2
     else:
-        random_case()
-        return
+        score = random_case(score)
+        return score
     tab[number_x][number_y] = number  # Entre la valeur déterminée au hasard dans une case vide
-    return # Condition pour continuer = Ne pas perdre
+    return score # Condition pour continuer = Ne pas perdre
 
 
-def affichage():
+def affichage(score):
     """Fonction qui lis le tableau(tab) ,afin d'associer à chaque nombre son image
     correspondante et l'affiche, de plus elle gère le score"""
-    score = 0 #on initialise le score
     fenetre.blit(image_dict.get("interface"), (0, 0))  # Affiche la grille de jeu
     for x in range(0, 5):  # boucle qui parcours le tableau
         for y in range(0, 5):
-            score = score + tab[x][y] # le score équivaut à la somme de toutes les cases
             if tab[x][y] != 0:
                 dispcoord = coords(x, y)  # Stocke dans la variable dispcoord l'equivalent en coordonnées dans le plan à partir des coordonnées dans le tableau
                 key = "case" + str(tab[x][y])  # "Créée" le nom de l'objet à afficher
@@ -148,6 +143,8 @@ try:
     ResX = 1280  # Resolution écran horizontale
     ResY = 720  # Resolution écran verticale
     pygame.font.init()
+    score = 0 #on initialise le score
+    controle = 0 # Controle sert à determiner si la touche pressée est utilisée dans les controles
     exceptions = [1, 3, 5, 9, 17, 33, 65, 129, 257, 513, 1025, 2049]
     # liste qui contient les exceptions pour les mouvements
     cons = [0] * 5  # Création du tableau contenant les cases
@@ -160,7 +157,7 @@ try:
     pygame.key.set_repeat(400, 30)
     image_dict = images_load()  # Fonction qui charge toutes les images
     #elle est utilisée dans la fonction random_case
-    affichage()
+    affichage(score)
     for g in range(5): # debug
         print(tab[g])  # debug
     while continuer: # boucle while principale
@@ -170,29 +167,35 @@ try:
             elif event.type == KEYDOWN:
                 if event.key == K_DOWN:
                    mouvement = move("down") #on demande de tenter de déplacer les cases vers le bas, si au moins une case bouge mouvement = 1
+                   controle = 1 # la touche est reconnue par le programme
                 elif event.key == K_UP:
                    mouvement = move("up")
+                   controle = 1
                 elif event.key == K_LEFT:
                    mouvement = move("left")
+                   controle = 1
                 elif event.key == K_RIGHT:
                    mouvement = move("right")
+                   controle = 1
                 elif event.key == K_RETURN and victoireoudefaite == 2: # en cas de défaite et d'appuie sur entrer
                    for i in range(5): #on réinitialise le tableau
                        tab[i] = list(cons)
+                else:
+                    controle = 0 # la touche n'est pas reconnue par le programme donc controle = 0
             if event.type == KEYUP:
                 if event.key == K_LEFT or K_RIGHT or K_UP or K_DOWN:
                     victoireoudefaite = vic_def(mouvement) # on vérifie la victoire ou défaite
                     if victoireoudefaite == 1: # si victoire
                         print("c'est gagné")
-                        random_case() #crée une case aléatoire
-                        affichage()
+                        score = random_case(score) #crée une case aléatoire
+                        affichage(score)
                     elif victoireoudefaite == 2: # si défaite
                         print("c'est perdu")
                         fenetre.blit(image_dict.get("defaite"), (0,0)) # Affiche le menu de défaite
                         pygame.display.flip()
-                    elif victoireoudefaite == 0: #si ni victoire ou défaite
-                        random_case() #crée une case aléatoire
-                        affichage()
+                    elif victoireoudefaite == 0 and controle == 1: #si ni victoire ou défaite et touche reconnue
+                        score = random_case(score) #crée une case aléatoire
+                        affichage(score)
 
 
 
