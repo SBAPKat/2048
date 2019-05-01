d570ffbc0158f3bc3a64ff532efc8bcf151129f1
SBAPKat <sbapkat@gmail.com>
2019-05-01 15:53:27 +0000

    new upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,12 @@
-git add *
+read -p "Enter name of file to upload, if you want to do a global upload, type *" name
+if [ "$name" = "*" ]
+then
+    echo "uploading everything"
+    git add *
+else
+    echo "$name is getting uploaded"
+    git add $name
+fi
 read -p "Enter Changelog: " changes
 git commit -m " $changes "
 git push origin master
