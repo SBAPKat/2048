832a9c7b46d8a9ce52d257eef69644b49a45bbf8
SBAPKat <sbapkat@gmail.com>
2019-05-02 19:39:23 +0000

    SBAPKat

--- upload.sh
+++ upload.sh
@@ -1,11 +1,11 @@
-read -p "Type B to create a new branch (in case of uploading error, or in case of multiple people working at the same time) or type R for a regular upload:" regular
+read -p "Type B to create a new branch or type R for a regular upload (please only do when you're sure others are not working on the project at the same time, to be safe, type B) :" regular
 if [ "$regular" = "B" ]
 then
     echo "selecting Branch Upload"
     read -p "Type name of branch (please use date, or something explicit):" branch_name
     git checkout -b $branch_name
 else
-    echo "selecting upload to main branch"
+    echo "selecting upload to main branch "
     branch_name="master"
 fi
 echo "uploading"
